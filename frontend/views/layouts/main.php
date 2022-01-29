538dea47785368b1614cf4b62bc323d79d7801f2
samir <samiredch@gmail.com>
2022-01-29 12:33:57 +0400

    icon

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,7 +36,7 @@ $activeCategory = Yii::$app->controller->id == 'categories' ? (Yii::$app->contro
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <?php $this->registerCsrfMetaTags() ?>
   <title><?= Html::encode($this->title) ?> | Aypara Post</title>
-
+  <link rel="icon" href="/icon.png" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
   <?php $this->head() ?>
 </head>
